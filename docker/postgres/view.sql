07ada00375791a1c68b167c6c1deb314a89390a4
KevinRond <ronk2602@usherbrooke.ca>
2023-06-08 12:20:53 -0400

    manque juste CIP ggs

--- docker/postgres/view.sql
+++ docker/postgres/view.sql
@@ -7,13 +7,13 @@ SELECT
     C.Sigle,
     C.Nom_cours,
     Ev.Nom_evaluation,
-    COALESCE(MAX(CASE WHEN Co.nom_comp = 'Comp1' THEN EC.Resultat END), 0) AS Comp1,
-    COALESCE(MAX(CASE WHEN Co.nom_comp = 'Comp1' THEN EC.Ponderation_competence END), 0) AS Ponderation_Comp1,
-    COALESCE(MAX(CASE WHEN Co.nom_comp = 'Comp2' THEN EC.Resultat END), 0) AS Comp2,
-    COALESCE(MAX(CASE WHEN Co.nom_comp = 'Comp2' THEN EC.Ponderation_competence END), 0) AS Ponderation_Comp2,
-    COALESCE(MAX(CASE WHEN Co.nom_comp = 'Comp3' THEN EC.Resultat END), 0) AS Comp3,
-    COALESCE(MAX(CASE WHEN Co.nom_comp = 'Comp3' THEN EC.Ponderation_competence END), 0) AS Ponderation_Comp3,
-    ROUND((SUM(CASE WHEN Co.nom_comp IN ('Comp1', 'Comp2', 'Comp3') THEN EC.Resultat ELSE 0 END) / SUM(EC.Ponderation_competence)) * 100) AS Total
+    MAX(CASE WHEN Co.nom_comp = 'Comp1' THEN EC.Resultat END) AS Comp1,
+    MAX(CASE WHEN Co.nom_comp = 'Comp1' THEN EC.Ponderation_competence END) AS Ponderation_Comp1,
+    MAX(CASE WHEN Co.nom_comp = 'Comp2' THEN EC.Resultat END) AS Comp2,
+    MAX(CASE WHEN Co.nom_comp = 'Comp2' THEN EC.Ponderation_competence END) AS Ponderation_Comp2,
+    MAX(CASE WHEN Co.nom_comp = 'Comp3' THEN EC.Resultat END) AS Comp3,
+    MAX(CASE WHEN Co.nom_comp = 'Comp3' THEN EC.Ponderation_competence END) AS Ponderation_Comp3,
+    ROUND((SUM(CASE WHEN Co.nom_comp IN ('Comp1', 'Comp2', 'Comp3') THEN EC.Resultat ELSE 0 END) / NULLIF(SUM(EC.Ponderation_competence), 0)) * 100) AS Total
 FROM
     ETUDIANT E
         JOIN ETUDIANTEVALUATION EE ON E.Cip = EE.Cip
--- docker/webserver/html/usager/script_request.js
+++ docker/webserver/html/usager/script_request.js
@@ -52,7 +52,7 @@ function requestCours() {
     trimestreSelect.addEventListener('change', requestCours);
     const selectedTrimestre = trimestreSelect.value;
     console.log(selectedTrimestre);
-    axios.get(`http://localhost:8888/api/selectinfo/gerz0501/${selectedTrimestre}`)
+    axios.get("http://localhost:8888/api/selectinfo/gerz0501/A22")
         .then(function (response) {
             console.log("Response: ", response.status);
             const coursData = response.data;
@@ -66,8 +66,10 @@ function requestCours() {
                 Object.entries(groupedData).forEach(([nomCours, coursArray]) => {
                     // Create a table element
                     const table = document.createElement('table');
-                    // Create table headers
-                    const headers = Object.keys(coursArray[0]);
+
+                    // Create table headers in the desired order
+                    const headers = ["Activité", "Nom Évaluation", "C1", "C2", "C3", "Total"];
+
                     const headerRow = document.createElement('tr');
                     headers.forEach(header => {
                         const th = document.createElement('th');
@@ -77,47 +79,78 @@ function requestCours() {
                     table.appendChild(headerRow);
 
                     // Create table rows
-                    coursArray.forEach(cours => {
+                    coursArray.forEach((cours, index) => {
                         const row = document.createElement('tr');
 
+                        if (index === 0) {
+                            const sigleNomCoursCell = document.createElement('td');
+                            sigleNomCoursCell.innerHTML = `<div style="text-align: center;"><strong>${cours.sigle}</strong></div><div style="text-align: center;" class="small">${cours.nomCours}</div>`;
+                            sigleNomCoursCell.rowSpan = coursArray.length;
+                            row.appendChild(sigleNomCoursCell);
+                        }
+
+                        const nomEvaluationCell = document.createElement('td');
+                        nomEvaluationCell.textContent = cours.nomEvaluation;
+                        row.appendChild(nomEvaluationCell);
+
                         const comp1Cell = document.createElement('td');
-                        comp1Cell.textContent = cours.sigle;
+                        const comp1Value = getCoursCellValue(cours.comp1, cours.ponderationComp1);
+                        const comp1Div = document.createElement('div');
+                        comp1Div.style.display = 'flex';
+                        comp1Div.style.justifyContent = 'center'; // Center align the content
+                        const comp1ValueSpan = document.createElement('span');
+                        comp1ValueSpan.style.fontWeight = 'bold';
+                        comp1ValueSpan.textContent = comp1Value.value !== '' ? comp1Value.value : ' ';
+                        const comp1PonderationSpan = document.createElement('span');
+                        comp1PonderationSpan.textContent = comp1Value.ponderation !== '' ? ` / ${comp1Value.ponderation}` : '';
+                        comp1Div.appendChild(comp1ValueSpan);
+                        comp1Div.innerHTML += ' ' + comp1PonderationSpan.textContent;
+                        comp1Cell.appendChild(comp1Div);
                         row.appendChild(comp1Cell);
 
                         const comp2Cell = document.createElement('td');
-                        comp2Cell.textContent = cours.nomCours;
+                        const comp2Value = getCoursCellValue(cours.comp2, cours.ponderationComp2);
+                        const comp2Div = document.createElement('div');
+                        comp2Div.style.display = 'flex';
+                        comp2Div.style.justifyContent = 'center'; // Center align the content
+                        const comp2ValueSpan = document.createElement('span');
+                        comp2ValueSpan.style.fontWeight = 'bold';
+                        comp2ValueSpan.textContent = comp2Value.value !== '' ? comp2Value.value : ' ';
+                        const comp2PonderationSpan = document.createElement('span');
+                        comp2PonderationSpan.textContent = comp2Value.ponderation !== '' ? ` / ${comp2Value.ponderation}` : '';
+                        comp2Div.appendChild(comp2ValueSpan);
+                        comp2Div.innerHTML += ' ' + comp2PonderationSpan.textContent;
+                        comp2Cell.appendChild(comp2Div);
                         row.appendChild(comp2Cell);
 
                         const comp3Cell = document.createElement('td');
-                        comp3Cell.textContent = cours.nomEvaluation;
+                        const comp3Value = getCoursCellValue(cours.comp3, cours.ponderationComp3);
+                        const comp3Div = document.createElement('div');
+                        comp3Div.style.display = 'flex';
+                        comp3Div.style.justifyContent = 'center'; // Center align the content
+                        const comp3ValueSpan = document.createElement('span');
+                        comp3ValueSpan.style.fontWeight = 'bold';
+                        comp3ValueSpan.textContent = comp3Value.value !== '' ? comp3Value.value : ' ';
+                        const comp3PonderationSpan = document.createElement('span');
+                        comp3PonderationSpan.textContent = comp3Value.ponderation !== '' ? ` / ${comp3Value.ponderation}` : '';
+                        comp3Div.appendChild(comp3ValueSpan);
+                        comp3Div.innerHTML += ' ' + comp3PonderationSpan.textContent;
+                        comp3Cell.appendChild(comp3Div);
                         row.appendChild(comp3Cell);
 
-                        const nomCoursCell = document.createElement('td');
-                        nomCoursCell.textContent = cours.comp1;
-                        row.appendChild(nomCoursCell);
-
-                        const nomEvaluationCell = document.createElement('td');
-                        nomEvaluationCell.textContent = cours.ponderationComp1;
-                        row.appendChild(nomEvaluationCell);
-
-                        const ponderationComp1Cell = document.createElement('td');
-                        ponderationComp1Cell.textContent = cours.comp2;
-                        row.appendChild(ponderationComp1Cell);
-
-                        const ponderationComp2Cell = document.createElement('td');
-                        ponderationComp2Cell.textContent = cours.ponderationComp2;
-                        row.appendChild(ponderationComp2Cell);
-
-                        const ponderationComp3Cell = document.createElement('td');
-                        ponderationComp3Cell.textContent = cours.comp3;
-                        row.appendChild(ponderationComp3Cell);
-
-                        const sigleCell = document.createElement('td');
-                        sigleCell.textContent = cours.ponderationComp3;
-                        row.appendChild(sigleCell);
-
                         const totalCell = document.createElement('td');
-                        totalCell.textContent = cours.total;
+                        const totalValue = getCoursCellValue(cours.total);
+                        const totalDiv = document.createElement('div');
+                        totalDiv.style.display = 'flex';
+                        totalDiv.style.justifyContent = 'center'; // Center align the content
+                        const totalValueSpan = document.createElement('span');
+                        totalValueSpan.style.fontWeight = 'bold';
+                        totalValueSpan.textContent = totalValue.value !== '' ? totalValue.value : ' ';
+                        const totalPonderationSpan = document.createElement('span');
+                        totalPonderationSpan.textContent = "%"; // Add the "%" sign
+                        totalDiv.appendChild(totalValueSpan);
+                        totalDiv.appendChild(totalPonderationSpan);
+                        totalCell.appendChild(totalDiv);
                         row.appendChild(totalCell);
 
                         table.appendChild(row);
@@ -137,9 +170,20 @@ function requestCours() {
         });
 }
 
+
 function groupByNomCours(coursData) {
     return coursData.reduce((result, cours) => {
         (result[cours.nomCours] = result[cours.nomCours] || []).push(cours);
         return result;
     }, {});
+}
+
+function getCoursCellValue(value, ponderation) {
+    if (value === null || ponderation === null) {
+        return { value: '', ponderation: '' }; // Empty cell
+    } else {
+        const formattedValue = value !== undefined ? value : '';
+        const formattedPonderation = ponderation !== undefined ? ponderation : '';
+        return { value: formattedValue, ponderation: formattedPonderation };
+    }
 }
