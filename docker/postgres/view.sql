6203999772250c8d1db578fd6538a5994225aabf
raphaelbarrette <122497610+raphaelbarrette@users.noreply.github.com>
2023-06-20 16:56:26 -0400

    view_cote_z x3

--- docker/postgres/view.sql
+++ docker/postgres/view.sql
@@ -1,5 +1,8 @@
 DROP VIEW IF EXISTS vue_notes_etudiant_trimestre CASCADE;
 DROP VIEW IF EXISTS vue_note_total cascade ;
+DROP VIEW IF EXISTS vue_cote_z_cours cascade ;
+DROP VIEW IF EXISTS vue_cote_z_trimestre cascade ;
+DROP VIEW IF EXISTS vue_cote_z_tot cascade ;
 
 CREATE VIEW vue_notes_etudiant_trimestre AS
 SELECT
@@ -42,3 +45,67 @@ FROM
         JOIN COURS C ON Co.Sigle = C.Sigle
 GROUP BY
     E.Cip, C.id_trimestre, C.Sigle;
+
+CREATE VIEW vue_cote_z_cours AS
+SELECT
+    VNT.cip,
+    VNT.id_trimestre,
+    VNT.sigle,
+    VNT.note_total,
+
+    CASE
+        WHEN Note_Total > 85 THEN 'A+'
+        WHEN Note_Total >= 81 AND Note_Total <= 84 THEN 'A'
+        WHEN Note_Total >= 78 AND Note_Total <= 80 THEN 'A-'
+        WHEN Note_Total >= 75 AND Note_Total <= 77 THEN 'B+'
+        WHEN Note_Total >= 71 AND Note_Total <= 74 THEN 'B'
+        WHEN Note_Total >= 68 AND Note_Total <= 70 THEN 'B-'
+        WHEN Note_Total >= 64 AND Note_Total <= 67 THEN 'C+'
+        WHEN Note_Total >= 60 AND Note_Total <= 63 THEN 'C'
+        WHEN Note_Total >= 57 AND Note_Total <= 59 THEN 'C-'
+        WHEN Note_Total >= 53 AND Note_Total <= 56 THEN 'D+'
+        WHEN Note_Total >= 50 AND Note_Total <= 52 THEN 'D'
+        ELSE 'E'
+        END AS cote_z_lettre,
+
+    CASE
+        WHEN Note_Total > 85 THEN 4.3
+        WHEN Note_Total >= 81 AND Note_Total <= 84 THEN 4.0
+        WHEN Note_Total >= 78 AND Note_Total <= 80 THEN 3.7
+        WHEN Note_Total >= 75 AND Note_Total <= 77 THEN 3.3
+        WHEN Note_Total >= 71 AND Note_Total <= 74 THEN 3.0
+        WHEN Note_Total >= 68 AND Note_Total <= 70 THEN 2.7
+        WHEN Note_Total >= 64 AND Note_Total <= 67 THEN 2.3
+        WHEN Note_Total >= 60 AND Note_Total <= 63 THEN 2.0
+        WHEN Note_Total >= 57 AND Note_Total <= 59 THEN 1.7
+        WHEN Note_Total >= 53 AND Note_Total <= 56 THEN 1.3
+        WHEN Note_Total >= 50 AND Note_Total <= 52 THEN 1.0
+        ELSE 0
+        END AS cote_z_chiffre
+FROM vue_note_total VNT;
+
+
+CREATE VIEW vue_cote_z_trimestre AS
+SELECT
+    VCZC.cip,
+    VCZC.id_trimestre,
+    ROUND(AVG(VCZC.cote_z_chiffre), 2) AS cote_z_trimestre
+
+
+FROM
+    vue_cote_z_cours VCZC
+GROUP BY
+    VCZC.cip,
+    VCZC.id_trimestre;
+
+CREATE VIEW vue_cote_z_tot AS
+SELECT
+    VCZT.cip,
+    ROUND(AVG(VCZT.cote_z_trimestre), 2) AS cote_z_total
+
+
+FROM
+    vue_cote_z_trimestre VCZT
+
+GROUP BY
+    VCZT.cip
